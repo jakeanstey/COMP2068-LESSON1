3d1347687ea3f075a3f5c15ffb87bf3a039abee8
Your NameJake <jake@miotigames.com>
2016-09-07 13:50:05 -0400

    Changed colon

--- nonblocking.js
+++ nonblocking.js
@@ -4,7 +4,7 @@ var food = fs.readFile('food.txt', 'utf-8', function(err, food){
   console.log(food);
 });
 
-console.log('FOOD:');
+console.log('FOOD::');
 
 var drinks = fs.readFile('drinks.txt', 'utf-8', function(err, drinks){
   console.log('DRINKS:');
